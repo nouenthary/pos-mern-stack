d9259f492b7de8d3d498ac86af9ca274778bb056
nouenthary <tharynouen22@email.com>
2025-05-24 19:45:23 +0700

    first commits 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import UserForm from './components/UserForm';
 import UserList from './components/UserList';
 
-const API = process.env.REACT_APP_API_URL + '/api/users';
+const API = "http://34.60.11.1:5000"+ '/api/users';
 
 function App() {
   const [users, setUsers] = useState([]);
